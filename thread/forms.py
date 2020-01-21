5df3bb7f428c319161823548bd61bc8df12992f9
長嶋大地 <nagashimadaichi@nagashimadaichinoMacBook-Pro.local>
2020-01-21 16:26:37 +0900

    1-14

---
+++ thread/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from .models import Topic
+
+class TopicCreateForm(ModelForm):
+    class Meta:
+        model = Topic
+        fields = [
+            "title",
+            "user_name",
+            "category",
+            "message",
+        ]
+
+
--- thread/views.py
+++ thread/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, FormView, DetailView
-
 from . models import Topic
+from . forms import TopicCreateForm
 
 
 class TopicDetailView(DetailView):
     template_name = 'thread/detail_topic.html'
     model = Topic
     context_object_name = 'topic'
+
+
+def topic_create(request):
+    template_name = "thread/create_topic.html"
+    ctx = {}
+
+    if request.method == "GET":
+        ctx["form"] = TopicCreateForm()
+        return render(request, template_name, ctx)
+
+    if request.method == "POST":
+        topic_form = TopicCreateForm(request.POST)
+        if topic_form.is_valid():
+            topic_form.save()
+            return redirect(reverse_lazy("base:top"))
+        else:
+            ctx["form"] = topic_form
+            return render(request, template_name, ctx)
+
+
